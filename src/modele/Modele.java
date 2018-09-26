dd80625985a57fa87a9d93b012215b18216bb974
levy54u <damienlevy546@gmail.com>
2018-09-26 11:33:07 +0200

    +modele

---
+++ src/modele/Modele.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package modele;
+
+/**
+ * @author levy54u
+ *
+ */
+public class Modele {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
