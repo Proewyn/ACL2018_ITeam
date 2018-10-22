a8fa9bf1846ae63dae2965096983539797c57b95
Damien Choffe <choffe5u@fst-ats-319-l09.fst.site.univ-lorraine.fr>
2018-10-22 12:51:12 +0200

    ajout de bordure

--- src/Generateur/Generateur.java
+++ src/Generateur/Generateur.java
@@ -17,8 +17,8 @@ public class Generateur {
 	private boolean[][] traverser;
 
 	public Generateur(int sizeX , int sizeY) {
-		lab = new Case[sizeX][sizeY];
-		traverser = new boolean[sizeX][sizeY];
+		lab = new Case[sizeX-2][sizeY-2];
+		traverser = new boolean[sizeX-2][sizeY-2];
 	}
 
 	
@@ -46,6 +46,7 @@ public class Generateur {
 				}
 			}
 		}
+		addBordure();
 	}
 
 	public void genererRetour() {
@@ -135,5 +136,22 @@ public class Generateur {
 		}
 	}
 
+	public void addBordure(){
+		Case[][] lab2 = new Case[lab.length+1][lab[0].length+1];
+		for (int i = 0; i < lab.length; i++) {
+			for (int j = 0; j < lab[0].length; j++) {
+				lab2[i+1][j+1] = lab[i][j];
+			}
+		}
+		for (int i = 0; i < lab2.length; i++) {
+			lab2[i][0] = new Mur(true);
+			lab2[i][lab2[0].length-1] = new Mur(true);
+		}
+		for (int i = 0; i < lab2[0].length; i++) {
+			lab2[0][i] = new Mur(true);
+			lab2[lab2.length-1][i] = new Mur(true);
+		}
+		lab = lab2;
+	}
 
 }
