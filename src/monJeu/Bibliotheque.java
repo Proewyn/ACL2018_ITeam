b5006c3e2fb2ecf612209bfe1ccf6cbfbd6d684a
ValentinCollignon <valentin.collignon@hotmail.fr>
2018-11-01 13:13:28 +0100

    ajout d'objet torche et téléporteur

--- src/monJeu/Bibliotheque.java
+++ src/monJeu/Bibliotheque.java
@@ -8,4 +8,9 @@ public class Bibliotheque {
 	public static final int SPAWN 	= 4;
 	public static final int ZOMBI   = 8;
 	
+	//id des objets
+	public static final int TORCHE = 21;
+	public static final int TELEPORTEUR = 22;
+	public static final int COFFRE = 23;
+		
 }
---
+++ src/objet/torche/Torche.java
@@ -0,0 +1,47 @@
+package objet.torche;
+
+import monJeu.Bibliotheque;
+import monJeu.Hero;
+import monJeu.MonJeu;
+import objet.Objet;
+
+public abstract class Torche implements Objet {
+	protected int x;
+	protected int y;
+	protected int id;
+	protected boolean pasTrouve;
+	public Torche(int x,int y) {
+		this.x=x;
+		this.y=y;
+		this.pasTrouve=true;
+		id = Bibliotheque.TORCHE;
+	}
+
+	@Override
+	public void Collision(MonJeu mj, int x, int y) {
+		if(pasTrouve && this.x==x && this.y==y)
+			this.action(mj);
+	}
+
+	
+	public boolean isPasTrouve() {
+		return pasTrouve;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	@Override
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public int getX() {
+		return x;
+	}
+	
+
+
+}
