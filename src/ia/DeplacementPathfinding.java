9b01fa65206afe867eb726f3ea283af6bdcd10a4
ValentinCollignon <valentin.collignon@hotmail.fr>
2018-11-27 18:51:19 +0100

    diagramme implementation + un peu correction de la classe pathinding

--- src/ia/DeplacementPathfinding.java
+++ src/ia/DeplacementPathfinding.java
@@ -96,30 +96,6 @@ public class DeplacementPathfinding implements DeplacementMonstre {
 		return poid;
 	}
 
-	class PointComp implements Comparator<Point>{
-
-		Point arriver;
-		public PointComp(Point arriver) {
-			// TODO Auto-generated constructor stub
-			this.arriver = arriver; 
-		}
-
-		@Override
-		public int compare(Point p1, Point p2) {
-			if( Distance(p1.x, p1.y, arriver.x, arriver.y) <  Distance(p2.x, p2.y, arriver.x, arriver.y)){
-				return 1;
-			} else {
-				return -1;
-			}
-		}
-
-		public double Distance(double x1, double y1, double x2, double y2) {
-			return Math.abs(x1- x2) + Math.abs( y1- y2);    
-			//return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
-		} 
-
-
-	}
 
 	public ArrayList<Point> getPointAccessible(Case[][] plateau, Point depart) {
 		ArrayList<Point> ptAccessible = new ArrayList<Point>();
